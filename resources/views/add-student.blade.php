501343bb16a1aac3556e5aade3f9a3cb670c1f25
unzila <unzila31@gmail.com>
2021-08-11 15:52:11 +0500

    Update add-student.blade.php

--- resources/views/add-student.blade.php
+++ resources/views/add-student.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 <div class="form">
+@if(Session::has('student_add'))
+<span>{{ Session::get('student_add')}}</span>
+@endif
 <form method="post" action="{{route('save.student')}}">
 @csrf
     <div class="form-input">
@@ -9,7 +12,7 @@
         <input class="form-control" type="text" value="" placeholder="Your Name" name="name"></br>
         <label>Email</label></br>
         <input class="form-control" type="email" value="" placeholder="Your Name" name="email"></br>
-        <input type="submit" name="submit">
+        <input type="submit" name="submit" class="form-submit">
     </div>
 </form>
 </div>
@@ -33,5 +36,12 @@
     border:1px solid green;
     padding:10px;
     width:100%;
+}
+.form-submit{
+    padding:5px;
+    border:1px solid black;
+    font-size:14px;
+    text-align:center;
+    margin-top:12px;
 }
 </style>
