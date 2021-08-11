d579241481a462ba84be3656eef4ee51e15c905a
unzila <unzila31@gmail.com>
2021-08-11 15:52:05 +0500

    Update StudentController.php

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class StudentController extends Controller
 {
     public function addstudent(){
         return view('add-student');
     }
-    public function savestudent(){
+    public function savestudent(Request $request){
+        DB::table('students')->insert([
+            'name'=>$request->name,
+            'email'=>$request->email
+        ]);
+        return back()->with('student_add','Student added succesfully');
+    }
+    public function studentslist(){
+        $students = DB::table('students')->get();
+        return view('student-list',compact('students'));
     }
 
 }
